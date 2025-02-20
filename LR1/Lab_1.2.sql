388a8ea2bf0a301b96725af973d73bc4d7ffc9e7
Stepanova-Anna <159694466+Stepanova-Anna@users.noreply.github.com>
2025-02-21 00:52:52 +0300

    Add files via upload

---
+++ LR1/Lab_1.2.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM simpledb.newbd
+LIMIT 0, 1000
+
+-- Date: 2025-02-21 00:52
+*/
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (1,1,'sport','run','2025-02-20 21:20:13');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (2,2,'designer','draw','2025-02-20 21:20:15');
+INSERT INTO `` (`resumeid`,`userid`,`title`,`skills`,`created`) VALUES (3,3,'ivt','programming','2025-02-20 21:20:15');
