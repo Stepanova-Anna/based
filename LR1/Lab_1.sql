0ab9504859edef7be3f8c2f2da992524a29ce653
Stepanova-Anna <159694466+Stepanova-Anna@users.noreply.github.com>
2025-02-21 00:38:34 +0300

    Add files via upload

---
+++ LR1/Lab_1.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM simpledb.users
+LIMIT 0, 1000
+
+-- Date: 2025-02-21 00:38
+*/
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,` created`) VALUES (1,'Ekaterina','ekaterina.petrova@outlook.com','F','2000-02-11','145789',1.123,'2025-02-20 21:20:13');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,` created`) VALUES (2,'Paul','paul@superpochta.ru','M','1998-08-12','123789',1,'2025-02-20 21:20:13');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,` created`) VALUES (3,'Jack','pupa@pochta.com','M','2000-01-01','145790',4,'2025-02-20 21:20:13');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,` created`) VALUES (4,'Poll','lupa@pocta.com','M','1998-03-08','156713',3,'2025-02-20 21:20:13');
+INSERT INTO `` (`id`,`name`,`email`,`gender`,`bday`,`postal_code`,`rating`,` created`) VALUES (5,'Eve','vanila@pochta.com','F','2001-11-17','128905',5,'2025-02-20 21:20:13');
